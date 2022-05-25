f58c6119aa9ec3300c496ad1de8c2cebe856cb60
JoJo-fr <geoffrey.auzou@gmail.com>
2022-05-25 15:24:23 +0200

    Fix ide

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import classes.Graphe;
+package src;
 
 public class Main {
 
     public static void main(String[] args) {
-        Graphe g1 = Graphe.creerGraphe("Orienté", 0);
-        Graphe g2 = Graphe.creerGraphe("Non orienté", 0);
+        src.classes.Graphe g1 = src.classes.Graphe.creerGraphe("Orienté", 0);
+        src.classes.Graphe g2 = src.classes.Graphe.creerGraphe("Non orienté", 0);
         System.out.println(g1);
         System.out.println(g2);
     }
