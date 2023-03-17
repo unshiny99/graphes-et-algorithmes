9832ee374d65b60eaddbbed42d633522b3f2c3c8
bryan <bryan@DESKTOP-PO3Q5D1.local>
2023-03-17 10:20:41 +0100

    Modification menu

--- source_tp_fisa3/src/Main.java
+++ source_tp_fisa3/src/Main.java
@@ -8,12 +8,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner scan_menu = new Scanner(System.in);
         Scanner scan = new Scanner(System.in);
-        
+
         Integer select = null;
         Integer type = null;
         Integer nbSommet = null;
@@ -22,7 +21,7 @@ public class Main {
         Integer identifiant_a = null;
         Integer identifiant_b = null;
         Integer cout = null;
-        
+
         Double proba = null;
 
         String name_file = null;
@@ -33,44 +32,43 @@ public class Main {
         // lire les données depuis le dossier data
         Path dir = Paths.get("./data/");
 
-        do{
-            if(select != null){
+        do {
+            if (select != null) {
                 System.out.println("[Terminé]");
             }
 
             System.out.println("--------------------------------------------------------\n" +
-                               "|                     MENU TP GRAPHES                   |\n" +
-                               "---------------------------------------------------------\n" +
-                               "|1  : MODE : Afficher graphe                            |\n" +
-                               "|2  : MODE : Création graphe (avec type/nombre sommets) |\n" +
-                               "|3  : MODE : Ajout sommet                               |\n" +
-                               "|4  : MODE : Ajout arc                                  |\n" +
-                               "|5  : MODE : Adjdacent ?                                |\n" +
-                               "|6  : MODE : Charger un graphe                          |\n" +
-                               "|7  : MODE : Sauvegarde du graphe                       |\n" +
-                               "--------------------------------------------------------"
-                               );
+                    "|                     MENU TP GRAPHES                   |\n" +
+                    "---------------------------------------------------------\n" +
+                    "|1  : MODE : Afficher graphe                            |\n" +
+                    "|2  : MODE : Création graphe (avec type/nombre sommets) |\n" +
+                    "|3  : MODE : Ajout sommet                               |\n" +
+                    "|4  : MODE : Ajout arc                                  |\n" +
+                    "|5  : MODE : Sont adjacents ?                           |\n" +
+                    "|6  : MODE : Charger un graphe                          |\n" +
+                    "|7  : MODE : Sauvegarde du graphe                       |\n" +
+                    "--------------------------------------------------------");
             try {
                 select = scan_menu.nextInt();
-            } catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println("Erreur de paramètre d'entrée.");
                 scan_menu.close();
                 scan.close();
                 break;
             }
-            switch(select){
+            switch (select) {
                 case 0:
                     scan_menu.close();
                     scan.close();
                     break;
-                
+
                 default:
                     select = 0;
                     scan_menu.close();
                     scan.close();
-                break;
+                    break;
             }
-        } while(select != 0);
+        } while (select != 0);
 
         scan_menu.close();
         scan.close();
