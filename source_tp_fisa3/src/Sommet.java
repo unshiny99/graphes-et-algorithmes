98c34bbaad16c98cae7a66ea02377bbec7baf3b6
Maxime Frémeaux <maxime.fremeaux2@gmail.com>
2023-05-02 16:44:50 +0200

    ajout nouvelles classes partie 4

---
+++ source_tp_fisa3/src/Sommet.java
@@ -0,0 +1,48 @@
+package src;
+
+public class Sommet implements Comparable<Sommet> {
+    private Integer index;
+    private Double poids;
+    private Integer profit;
+
+    public Sommet(Integer index, Double poids, Integer profit) {
+        this.index = index;
+        this.poids = poids;
+        this.profit = profit;
+    }
+
+    @Override
+    public int compareTo(Sommet sommet) {
+        if(this.profit > sommet.getProfit()) {
+            return 1;
+        } else if(this.profit < sommet.getProfit()) {
+            return -1;
+        } else { // this.profit = sommet.getProfit()
+            return 0;
+        }
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public Double getPoids() {
+        return poids;
+    }
+
+    public void setPoids(Double poids) {
+        this.poids = poids;
+    }
+
+    public Integer getProfit() {
+        return profit;
+    }
+
+    public void setProfit(Integer profit) {
+        this.profit = profit;
+    }
+}
