cea1450f3a5767bff2cc68e6043470ad1f863af6
Maxime Frémeaux <maxime.fremeaux2@gmail.com>
2023-05-04 10:47:41 +0200

    supp import inutilisés

--- source_tp_fisa3/src/Main.java
+++ source_tp_fisa3/src/Main.java
@@ -4,8 +4,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
